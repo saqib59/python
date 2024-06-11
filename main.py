92eb0767847d2a73ac5e8881210846a8272448c5
Saqib Ali <saqibali80400@gmail.com>
2024-06-12 00:47:43 +0500

    day 6

---
+++ main.py
@@ -0,0 +1,13 @@
+'''
+This is a comment
+Author
+'''
+# print("hey my name is \"saqib\" \nI am 26 years old")
+
+# print("how", "are", "you", sep="~")
+
+c = ''
+# print("type of c ", type(c))
+
+a = complex(8, 2);
+print(a)
