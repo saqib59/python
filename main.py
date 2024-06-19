ee491fe3e2d14d6af13a472a7466a0be43250ad1
Saqib Ali <saqibali80400@gmail.com>
2024-06-19 15:15:01 +0500

    string slicing

--- main.py
+++ main.py
@@ -2,32 +2,17 @@
 This is a comment
 Author
 '''
-# print("hey my name is \"saqib\" \nI am 26 years old")
 
-# print("how", "are", "you", sep="~")
+name = "Saqib"
+nameLeng = len(name)
+# print(names[0:5])
+print(name[0:4]) # including 0 but not 4
+print(name[1:4]) # including 1 but not 4
+print(name[:5]) 
+print(name[0 : -3]) 
+print(name[:nameLeng-3]) 
 
-# c = ''
-# # print("type of c ", type(c))
+name = "Harry"
+print(name[-4 : -2])
 
-# a = complex(8, 2);
-# print(a)
-
-# num1 = 10
-# num2 = 2
-
-# print("Addition of the two numbers {num1} and {num2} = {num1 + num2}")
-
-# Input
-
-# name = input("Enter your name: ")
-
-# print("your name is" , name)
-print('''he said: 
-"I want to eat an apple " ''')
-
-
-myName = "Saqib"
-
-for character in myName:
-    print(character)
 
