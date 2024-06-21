ec1218b7c69f4555ccc590f615965fc3ac503795
Saqib Ali <saqibali80400@gmail.com>
2024-06-21 15:33:22 +0500

    string methods

--- main.py
+++ main.py
@@ -3,16 +3,10 @@ This is a comment
 Author
 '''
 
-name = "Saqib"
-nameLeng = len(name)
-# print(names[0:5])
-print(name[0:4]) # including 0 but not 4
-print(name[1:4]) # including 1 but not 4
-print(name[:5]) 
-print(name[0 : -3]) 
-print(name[:nameLeng-3]) 
+name = "Saqib Ali"
 
-name = "Harry"
-print(name[-4 : -2])
+# print(name.rstrip("!").upper().replace("S", ""))
 
+print(name.center(100))
 
+print(name.capitalize())
