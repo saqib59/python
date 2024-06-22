e165ae06b8b29856ec9c61ff57889a9d6d8c67fd
Saqib Ali <saqibali80400@gmail.com>
2024-06-22 15:36:56 +0500

    for loops

--- main.py
+++ main.py
@@ -3,16 +3,12 @@
 '''
 
 
-number = int(input("Enter the value of x: "))
+colors = ["Red", "Green", "Blue"];
 
-match (number):
-    case (3):
-        print("number is 3")
-    case (5):
-        print("number is 5")
-    case _ if number!=90:
-       print(f"{number} is not 90")
-    case _ if number!=80:
-       print(f"{number} is not 80")
-    case _:
-       print(number)
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+
+for number in range(5):
+    print(number)
