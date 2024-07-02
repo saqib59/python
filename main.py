afd3f39485a769b156124761aa7057bfaa6a0651
Saqib Ali <saqibali80400@gmail.com>
2024-07-03 00:53:26 +0500

    numbers

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-def chai(name):
-    print(name)
+import random
+l1 = ['lemon', 'apple', 'mango', 'banana']
 
-chai("karak chai")
+from decimal import Decimal
 
+random.shuffle(l1)
+print(random.choice(l1))
+print(l1)
 
-chai_one = "lemon tea"
+Decimal('0.1') + Decimal('0.1') + Decimal('0.1')
+
+set_one = {'saqib', 'ali', 'umer'}
+set_two = {'azan', 'ali', 'faiz'}
+
+result = set_one | set_two
+
+print(result)
