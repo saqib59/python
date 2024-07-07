d92e3602d5f55ad188a997f5b4359da37058bd04
Saqib Ali <saqibali80400@gmail.com>
2024-07-08 00:32:22 +0500

    dict

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
-chai = "Lemon, Ginger, Mint, Masala"
+# Dictionary
 
-# print(chai.split(", "))
-# print(chai)
+chai_types = { "Masala" : "Spicy", "Ginger" : "Zesty", "Green" : "Mild" }
 
-chai = "Masala Chai chai Chai"
+chai_types["Green"]  = "Fresh"
 
-# print(chai.count("Chai"))
-
-chai_types = ["Lemon", "Masala", "Ginner"]
+for chai in chai_types:
+    print(chai, chai_types[chai])
 
 
-print(", ".join(chai_types))
+for key, value in chai_types.items():
+    print(key, value)
+
+tea_shop = {
+    "chai" : {
+        "Masala" : "Spicy",
+        "Ginger" : "Zesty"
+    },
+    "tea" : {
+        "Green" : "Mild",
+        "Black" : "Strong"
+    }
+}
+
+print(tea_shop["chai"]["Masala"])
+
+squared_num = {x: x**2 for x in range(6)}
+
+
+squared_num.clear()
+
+print(squared_num)
+
+keys = ["Masala", "Ginger", "Lemon"]
+
+default_value = "Delicious"
+
+new_dict = dict.fromkeys(keys, default_value)
+
+print(new_dict)
