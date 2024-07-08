66076cb761251d4f627797bbc97852cab0091829
espark-saqib <saqibali@esparkconsultants.com>
2024-07-08 11:14:27 +0500

    tuple

--- main.py
+++ main.py
@@ -1,40 +1,10 @@
-# Dictionary
+# Tuple
 
-chai_types = { "Masala" : "Spicy", "Ginger" : "Zesty", "Green" : "Mild" }
+chai_types = ("Black", "Green", "Masala")
 
-chai_types["Green"]  = "Fresh"
+more_tea = ("Herbal", "Milk")
 
-for chai in chai_types:
-    print(chai, chai_types[chai])
+all_tea = chai_types + more_tea
 
-
-for key, value in chai_types.items():
-    print(key, value)
-
-tea_shop = {
-    "chai" : {
-        "Masala" : "Spicy",
-        "Ginger" : "Zesty"
-    },
-    "tea" : {
-        "Green" : "Mild",
-        "Black" : "Strong"
-    }
-}
-
-print(tea_shop["chai"]["Masala"])
-
-squared_num = {x: x**2 for x in range(6)}
-
-
-squared_num.clear()
-
-print(squared_num)
-
-keys = ["Masala", "Ginger", "Lemon"]
-
-default_value = "Delicious"
-
-new_dict = dict.fromkeys(keys, default_value)
-
-print(new_dict)
+if "Green" in all_tea:
+    print("I have green tea")
